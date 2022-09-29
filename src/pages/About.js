500e9b8be9d1ed90c07e97cb82d068e3a5a7f815
parthav patil <patilparthav@gmail.com>
2022-09-30 03:50:04 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () =>{ 
+
+    return(
+
+   <div className="mb-20">
+    <h1 className="sm:text-4x1 text-2x1 font-bold mt-6 mb-6 text-gray-900">Welcome to my Blog!</h1>
+    About
+</div>
+    );
+};
+
+export default About;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <h1 className="sm:text-4x1 test-2x1 font-bold mt-6 text-gray-900">
+    The page you are looking for does not exist.
+    </h1>
+  )
+}
+
+export default NotFound;
+
